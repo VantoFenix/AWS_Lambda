04c7670754f6f27403d6344bf1d2d1defde899e0
bruno <vantofortnite@gmail.com>
2026-04-29 20:44:13 -0500

    feat: agregar colas sqs y configuracion de mensajes muertos

--- iac/s3_sqs.tf
+++ iac/s3_sqs.tf
@@ -5,7 +5,7 @@
 # Bucket principal para imágenes
 resource "aws_s3_bucket" "image_bucket" {
   bucket        = "proyectoaws-storage-${terraform.workspace}"
-  force_destroy = true # Permite borrar todo al final sin errores manuales
+  force_destroy = true 
 
   tags = { 
     Name        = "proyectoaws-bucket-${terraform.workspace}"
@@ -13,8 +13,8 @@ resource "aws_s3_bucket" "image_bucket" {
   }
 }
 
-# REGLA DE CICLO DE VIDA (Protección de costos)
-# Borra automáticamente cualquier archivo después de 1 día.
+
+# configuracon para borrar automáticamente cualquier archivo después de 1 dia
 resource "aws_s3_bucket_lifecycle_configuration" "cleanup" {
   bucket = aws_s3_bucket.image_bucket.id
 
@@ -24,5 +24,36 @@ resource "aws_s3_bucket_lifecycle_configuration" "cleanup" {
     expiration {
       days = 1
     }
+  }
+}
+
+
+
+#  SQS 
+
+# 1DLQ
+resource "aws_sqs_queue" "image_dlq" {
+  name = "proyectoaws-dlq-${terraform.workspace}"
+  tags = { 
+    Name        = "proyectoaws-dlq-${terraform.workspace}"
+    Environment = terraform.workspace
+  }
+}
+
+# 2 Cola Principal
+resource "aws_sqs_queue" "image_queue" {
+  name                      = "proyectoaws-queue-${terraform.workspace}"
+  delay_seconds             = 0
+  message_retention_seconds = 86400 
+  receive_wait_time_seconds = 10    
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.image_dlq.arn
+    maxReceiveCount     = 3 
+  })
+
+  tags = { 
+    Name        = "proyectoaws-queue-${terraform.workspace}"
+    Environment = terraform.workspace
   }
 }
